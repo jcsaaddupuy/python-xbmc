e2fe4e423f279492ab485b81e02437910bb6cace
Jean-Christophe Saad-Dupuy <saad.dupuy@gmail.com>
2013-03-31 00:28:07 +0100

    Clean xbmc.py test code

--- xbmc.py
+++ xbmc.py
@@ -73,15 +73,3 @@ class Gui(XbmcNamespace):
 class Player(XbmcNamespace):
 	pass
 
-if __name__ == "__main__":
-	xbmc = XBMC("http://ida/jsonrpc")
-	#xbmc.Application.SetVolume(100)
-#	xbmc.Application.SetMute(False)
-	#xbmc.Gui.ActivateWindow("weather")
-	#xbmc.Gui.ActivateWindow("home")
-	#xbmc.VideoLibrary.Scan()
-	#xbmc.VideoLibrary.Clean()
-	#xbmc.Player.PlayPause([PLAYER_VIDEO])
-	#xbmc.Player.Stop(PLAYER_VIDEO)
-	#xbmc.VideoLibrary.GetTVShows({ "filter": {"field": "playcount", "operator": "is", "value": "0"}}, id="libTvShows")
-	#print xbmc.VideoLibrary.GetTVShows({ "filter": {"field": "playcount", "operator": "is", "value": "0"}, "limits": { "start" : 0, "end": 75 }, "properties": ["art", "genre", "plot", "title", "originaltitle", "year", "rating", "thumbnail", "playcount", "file", "fanart"], "sort": { "order": "ascending", "method": "label" } }, id="libTvShows")
