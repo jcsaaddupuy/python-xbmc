7edf61cdcf1d4c08c062d97bfe6a85bba0192a65
Jean-Christophe Saad-Dupuy <saad.dupuy@gmail.com>
2013-10-27 14:06:08 +0100

    Updated examples

--- example.py
+++ example.py
@@ -4,15 +4,24 @@ from xbmc import XBMC, PLAYER_VIDEO
 
 if __name__ == "__main__":
 	xbmc = XBMC("http://YOURHOST/jsonrpc")
-	xbmc.Application.SetVolume(100)
-	xbmc.Application.SetMute(False)
-	xbmc.Gui.ActivateWindow("weather")
-	xbmc.Gui.ActivateWindow("home")
+        # JSON RPC
+        # Ping
+        print xbmc.JSONRPC.Ping()
+	
+        # Gui
+        xbmc.GUI.ActivateWindow({"window":"home"})
+        xbmc.GUI.ActivateWindow({"window":"weather"})
+        # Show a notification
+        xbmc.GUI.ShowNotification({"title":"Title", "message":"Hello notif"})
+        # Application
+        xbmc.Application.SetMute({"mute":True})	
+        xbmc.Application.SetMute({"mute":False})	
+        # Video library
 	xbmc.VideoLibrary.Scan()
 	xbmc.VideoLibrary.Clean()
+        # Query the video library
+	print xbmc.VideoLibrary.GetTVShows({ "filter": {"field": "playcount", "operator": "is", "value": "0"}, "limits": { "start" : 0, "end": 75 }, "properties": ["art", "genre", "plot", "title", "originaltitle", "year", "rating", "thumbnail", "playcount", "file", "fanart"], "sort": { "order": "ascending", "method": "label" } }, id="libTvShows")
+        
+        #Player
 	xbmc.Player.PlayPause([PLAYER_VIDEO])
 	xbmc.Player.Stop([PLAYER_VIDEO])
-	print xbmc.VideoLibrary.GetTVShows({ "filter": {"field": "playcount", "operator": "is", "value": "0"}, "limits": { "start" : 0, "end": 75 }, "properties": ["art", "genre", "plot", "title", "originaltitle", "year", "rating", "thumbnail", "playcount", "file", "fanart"], "sort": { "order": "ascending", "method": "label" } }, id="libTvShows")
-        # Show a notification
-        xbmc.Gui.ShowNotification({"title":"Title", "message":"Hello notif"})
-        print xbmc.JSONRPC.Ping()["result"]
