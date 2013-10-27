28a86543feefacf28c1d55302fcac8690e3b09e7
Jean-Christophe Saad-Dupuy <saad.dupuy@gmail.com>
2013-10-27 12:35:49 +0100

    Added example for Gui.ShowNotification

--- example.py
+++ example.py
@@ -13,3 +13,5 @@ if __name__ == "__main__":
 	xbmc.Player.PlayPause([PLAYER_VIDEO])
 	xbmc.Player.Stop([PLAYER_VIDEO])
 	print xbmc.VideoLibrary.GetTVShows({ "filter": {"field": "playcount", "operator": "is", "value": "0"}, "limits": { "start" : 0, "end": 75 }, "properties": ["art", "genre", "plot", "title", "originaltitle", "year", "rating", "thumbnail", "playcount", "file", "fanart"], "sort": { "order": "ascending", "method": "label" } }, id="libTvShows")
+        # Show a notification
+        xbmc.Gui.ShowNotification({"title":"Title", "message":"Hello notif"})
