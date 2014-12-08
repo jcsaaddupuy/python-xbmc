b0fa080738e5a9f21ba018d6280f50fb41a2c938
Jean-Christophe SAAD-DUPUY <jc.saaddupuy@fsfe.org>
2014-12-08 22:16:06 +0100

    python 2 and 3 urllib compatibility (see #4)

--- xbmcjson/xbmcjson.py
+++ xbmcjson/xbmcjson.py
@@ -1,9 +1,26 @@
 #!/bin/env python
 # VERSION = "0.0.4.dev"
 
-import urllib
 import json
 from io import StringIO
+
+# 2 vs 3 compatibility
+try:
+    # python 3
+    from urllib.request import HTTPPasswordMgrWithDefaultRealm
+    from urllib.request import HTTPBasicAuthHandler
+    from urllib.request import build_opener
+    from urllib.request import install_opener
+    from urllib.request import Request
+    from urllib.request import urlopen
+except:
+    # python 2
+    from urllib2 import HTTPPasswordMgrWithDefaultRealm
+    from urllib2 import HTTPBasicAuthHandler
+    from urllib2 import build_opener
+    from urllib2 import install_opener
+    from urllib2 import Request
+    from urllib2 import urlopen
 
 PLAYER_VIDEO=1
 
@@ -28,7 +45,7 @@ class XBMCJsonTransport(XBMCTransport):
     # Params are given as a dictionnary
     if len(args) == 1:
       args=args[0]
-      params = kwargs 
+      params = kwargs
       # Use kwargs for param=value style
     else:
       args = kwargs
@@ -41,15 +58,15 @@ class XBMCJsonTransport(XBMCTransport):
 
     values=json.dumps(params)
     # HTTP Authentication
-    password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm() 
-    password_mgr.add_password(None, self.url, self.username, self.password) 
-    auth_handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-    opener = urllib.request.build_opener(auth_handler) 
-    urllib.request.install_opener(opener)
+    password_mgr = HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, self.url, self.username, self.password)
+    auth_handler = HTTPBasicAuthHandler(password_mgr)
+    opener = build_opener(auth_handler)
+    install_opener(opener)
 
     data = values
-    req = urllib.request.Request(self.url, data.encode('utf-8'), header)
-    response = urllib.request.urlopen(req)
+    req = Request(self.url, data.encode('utf-8'), header)
+    response = urlopen(req)
     the_page = response.read()
     if len(the_page) > 0 :
       return json.load(StringIO(the_page.decode('utf-8')))
